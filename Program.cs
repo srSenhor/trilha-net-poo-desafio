537a778e36dc11044f9ea69853e12b295c1cdb74
srSenhor <viniciusfeliciano14@gmail.com>
2024-05-13 17:25:45 -0300

    fix: corrigido o que não tinha sido implementado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone1 = new Iphone("00 1234-5678", "15 Pro Máx", "001970-01-010000-0", 16);
+iphone1.Ligar();
+iphone1.InstalarAplicativo("Telegram");
+
+Console.WriteLine("----------------------------------\n");
+
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia1 = new Nokia("11 8765-4321", "G11 Plus", "001970-01-010000-0", 32);
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("Whatsapp");
